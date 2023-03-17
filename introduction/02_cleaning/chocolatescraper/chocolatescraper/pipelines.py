51cd62a40b0c40633761d6354d0b4b8415accc91
Nick <34078700+nhollingsworth09@users.noreply.github.com>
2023-03-17 12:49:23 -0400

    Class 02 Completed

--- introduction/02_cleaning/chocolatescraper/chocolatescraper/pipelines.py
+++ introduction/02_cleaning/chocolatescraper/chocolatescraper/pipelines.py
@@ -6,7 +6,45 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
 
+
+class PriceToUSDPipeline:
+    #Convert EUR to USD. Drop if price not available
+
+    gbpToUsdRate = 1.3
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        ## check is price present
+        if adapter.get('price'):
+
+            #converting the price to a float
+            floatPrice = float(adapter['price'])
+
+            #converting the price from gbp to usd using our hard coded exchange rate
+            adapter['price'] = floatPrice * self.gbpToUsdRate
+
+            return item
+
+        else:
+            # drop item if no price
+            raise DropItem(f"Missing price in {item}")
+
+class DuplicatesPipeline:
+    #Check for duplicated product names. Drop if already present. 
+
+    def __init__(self):
+        self.names_seen = set()
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        if adapter['name'] in self.names_seen:
+            raise DropItem(f"Duplicate item found: {item!r}")
+        else:
+            self.names_seen.add(adapter['name'])
+            return item
 
 class ChocolatescraperPipeline:
     def process_item(self, item, spider):
--- introduction/02_cleaning/chocolatescraper/chocolatescraper/spiders/chocolatespider.py
+++ introduction/02_cleaning/chocolatescraper/chocolatescraper/spiders/chocolatespider.py
@@ -1,30 +1,45 @@
 # -*- coding: utf-8 -*-
+"""
+For our tutorial we are going to configure our Item Loader to:
+
+- Remove the £ sign from the data we're scraping.
+- Convert the scraped relative urls to full absolute urls.
+
+We do this by using Scrapy itemloaders (preprocessors). Additionally, we'll be using pipelines to process the data before exporting by completing the below tasks:
+
+- Convert the price from a string to a float (so we can then multiply it by the exchange rate).
+- Convert the price from British Pounds to US Dollars
+- Drop items that currently have no price (due to being sold out).
+- Check if an Item is a duplicate and drop it if it's a duplicate.
+"""
+
 import scrapy
+from chocolatescraper.itemloaders import ChocolateProductLoader
+from chocolatescraper.items import ChocolateProduct  
 
+ 
+class ChocolateSpider(scrapy.Spider):
 
-class ChocolatespiderSpider(scrapy.Spider):
-    name = "chocolatespider"
-    allowed_domains = ["chocolate.co.uk"]
-    start_urls = ["https://www.chocolate.co.uk/collections/all"]
+   # The name of the spider
+   name = 'chocolatespider'
 
-    def parse(self, response):
-        
-        #here we loop through the products and extract name, price, and url
-        products = response.css('product-item')
-        
-        for product in products:
-            #here, we put the data returned into the format we want to output for our csv/json file
-            yield{
-                'name' : product.css('a.product-item-meta__title::text').get(),
-                'price' : product.css('span.price').get().replace('<span class="price">\n              <span class="visually-hidden">Sale price</span>£','').replace('</span>',''),
-                'url' : product.css('div.product-item-meta a').attrib['href']
-            }
-            
-            #follow the next page link
-            nav_items = response.css('a.pagination__nav-item')
-            
-            next_page = response.css('[rel="next"] ::attr(href)').get()
+   # These are the urls that we will start scraping
+   start_urls = ['https://www.chocolate.co.uk/collections/all']
 
-            if next_page is not None:
-                next_page_url = 'https://www.chocolate.co.uk' + next_page
-                yield response.follow(next_page_url, callback=self.parse)
+   def parse(self, response):
+       products = response.css('product-item')
+
+       for product in products:
+            chocolate = ChocolateProductLoader(item=ChocolateProduct(), selector=product)
+            chocolate.add_css('name', "a.product-item-meta__title::text")
+            chocolate.add_css('price', 'span.price', re='<span class="price">\n              <span class="visually-hidden">Sale price</span>(.*)</span>')
+            chocolate.add_css('url', 'div.product-item-meta a::attr(href)')
+            yield chocolate.load_item()
+
+       next_page = response.css('[rel="next"] ::attr(href)').get()
+
+       if next_page is not None:
+           next_page_url = 'https://www.chocolate.co.uk' + next_page
+           yield response.follow(next_page_url, callback=self.parse)
+           
+
