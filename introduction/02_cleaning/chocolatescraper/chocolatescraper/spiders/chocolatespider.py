0a4908028004571f45f459527cf77fe875d06614
Nick <34078700+nhollingsworth09@users.noreply.github.com>
2023-03-17 02:44:46 -0400

    Copy spider into cleaning lesson

---
+++ introduction/02_cleaning/chocolatescraper/chocolatescraper/spiders/chocolatespider.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class ChocolatespiderSpider(scrapy.Spider):
+    name = "chocolatespider"
+    allowed_domains = ["chocolate.co.uk"]
+    start_urls = ["https://www.chocolate.co.uk/collections/all"]
+
+    def parse(self, response):
+        
+        #here we loop through the products and extract name, price, and url
+        products = response.css('product-item')
+        
+        for product in products:
+            #here, we put the data returned into the format we want to output for our csv/json file
+            yield{
+                'name' : product.css('a.product-item-meta__title::text').get(),
+                'price' : product.css('span.price').get().replace('<span class="price">\n              <span class="visually-hidden">Sale price</span>£','').replace('</span>',''),
+                'url' : product.css('div.product-item-meta a').attrib['href']
+            }
+            
+            #follow the next page link
+            nav_items = response.css('a.pagination__nav-item')
+            
+            next_page = response.css('[rel="next"] ::attr(href)').get()
+
+            if next_page is not None:
+                next_page_url = 'https://www.chocolate.co.uk' + next_page
+                yield response.follow(next_page_url, callback=self.parse)
